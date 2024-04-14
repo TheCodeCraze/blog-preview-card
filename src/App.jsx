d23adb7abe993d7002d460616d1864e791dfb16b
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-04-14 20:48:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import styles from "./App.module.css";
+
+export function App() {
+  return (
+    <article className={styles.container}>
+      <img
+        className={styles.image}
+        src="illustration-article.svg"
+        alt="Article's illustration"
+      />
+      <div className={styles.info}>
+        <p className={styles.category}>Learning</p>
+        <time className={styles.date}>Published 21 Dec 2023</time>
+        <h1 className={styles.title}>HTML & CSS foundations</h1>
+        <p className={styles.description}>
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+      </div>
+      <div className={styles.author}>
+        <img
+          className={styles.pic}
+          src="image-avatar.svg"
+          alt="Greg Hooper's avatar"
+        />
+        <p className={styles.name}>Greg Hooper</p>
+      </div>
+    </article>
+  );
+}
